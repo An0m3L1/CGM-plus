76f77e4c805d9e8a8bde3db6a02c60a7f3b98cb8
An0m3L1 <velashevic@gmail.com>
2024-06-26 22:09:33 +0300

    Tweaks to the assault rifle reload animation.

--- src/main/java/com/mrcrayfish/guns/client/render/gun/model/AssaultRifleModel.java
+++ src/main/java/com/mrcrayfish/guns/client/render/gun/model/AssaultRifleModel.java
@@ -1,10 +1,14 @@
 package com.mrcrayfish.guns.client.render.gun.model;
 
+import java.lang.reflect.Method;
+import javax.annotation.Nullable;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mrcrayfish.guns.GunMod;
 import com.mrcrayfish.guns.client.handler.GunRenderingHandler;
 import com.mrcrayfish.guns.common.Gun;
 import com.mrcrayfish.guns.client.GunModel;
 import com.mrcrayfish.guns.client.SpecialModels;
+import com.mrcrayfish.guns.client.handler.ReloadHandler;
 import com.mrcrayfish.guns.client.render.gun.IOverrideModel;
 import com.mrcrayfish.guns.client.util.GunReloadAnimationHelper;
 import com.mrcrayfish.guns.client.util.RenderUtil;
@@ -26,6 +30,10 @@ import javax.annotation.Nullable;
  */
 public class AssaultRifleModel implements IOverrideModel
 {
+    private boolean disableAnimations = false;
+    private Method getReloadCycleProgress = null;
+    private Method getAnimationTrans = null;
+
     @Override
     public void render(float partialTicks, ItemTransforms.TransformType transformType, ItemStack stack, ItemStack parent, @Nullable LivingEntity entity, PoseStack poseStack, MultiBufferSource buffer, int light, int overlay)
     {
@@ -42,6 +50,36 @@ public class AssaultRifleModel implements IOverrideModel
             RenderUtil.renderModel(SpecialModels.ASSAULT_RIFLE_NO_SIGHTS.getModel(), transformType, null, stack, parent, poseStack, buffer, light, overlay);
         }
 
+        // Special animated segment for compat with the CGM Expanded fork.
+        boolean isPlayer = (entity != null && entity.equals(Minecraft.getInstance().player));
+        boolean isFirstPerson = (transformType == ItemTransforms.TransformType.FIRST_PERSON_RIGHT_HAND || transformType == ItemTransforms.TransformType.FIRST_PERSON_LEFT_HAND);
+
+        Vec3 transforms = Vec3.ZERO;
+
+        if(isPlayer && isFirstPerson && !disableAnimations)
+        {
+            try {
+                float reloadCycleProgress = GunRenderingHandler.get().getReloadCycleProgress(stack);
+                transforms = GunReloadAnimationHelper.getAnimationTrans(stack, reloadCycleProgress, "magazine").scale(ReloadHandler.get().getReloadProgress(partialTicks)).scale(0.5f);
+            }
+            catch(Exception e) {
+                GunMod.LOGGER.error("Redundant Guns encountered an error trying to apply animations:");
+                e.printStackTrace();
+                disableAnimations = true;
+            }
+        }
+
+        poseStack.pushPose();
+        // Now we apply our transformations.
+        // All we need to do is move the model based on the cooldown variable.
+        if(isPlayer && transforms!=Vec3.ZERO)
+            poseStack.translate(transforms.x*0.0625, transforms.y*0.0625, transforms.z*0.0625);
+        // Our transformations are done - now we can render the model.
+        RenderUtil.renderModel(SpecialModels.ASSAULT_RIFLE_MAG.getModel(), transformType, null, stack, parent, poseStack, buffer, light, overlay);
+        // Pop pose to compile everything in the render matrix.
+        poseStack.popPose();
+
+        /*
         //Moving magazine code
         poseStack.pushPose();
         float reloadProgress = GunRenderingHandler.get().getReloadCycleProgress(stack);
@@ -49,5 +87,6 @@ public class AssaultRifleModel implements IOverrideModel
         poseStack.translate(transform.x, transform.y, transform.z);
         RenderUtil.renderModel(SpecialModels.ASSAULT_RIFLE_MAG.getModel(), transformType,null, stack, parent, poseStack, buffer, light, overlay);
         poseStack.popPose();
+        */
     }
 }

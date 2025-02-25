60a1cca46ba1777772e4b567acdfacefbafadee1
An0m3L1 <93768372+An0m3L1@users.noreply.github.com>
2025-02-25 19:07:15 +0300

    Overhauled smoke grenade tooltip.

--- src/main/java/com/mrcrayfish/guns/item/SmokeGrenadeItem.java
+++ src/main/java/com/mrcrayfish/guns/item/SmokeGrenadeItem.java
@@ -4,6 +4,7 @@ import com.mrcrayfish.guns.Config;
 import com.mrcrayfish.guns.entity.ThrowableGrenadeEntity;
 import com.mrcrayfish.guns.entity.ThrowableSmokeGrenadeEntity;
 import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
@@ -27,13 +28,20 @@ public class SmokeGrenadeItem extends GrenadeItem
     @Override
     public void appendHoverText(@NotNull ItemStack stack, @Nullable Level worldIn, @NotNull List<Component> tooltip, @NotNull TooltipFlag flag)
     {
-        tooltip.add(Component.translatable("info.cgm.gun_details").withStyle(ChatFormatting.GOLD));
         double smokeDuration = Config.COMMON.explosives.smokeGrenadeCloudDuration.get();
         double smokeDiameter = Config.COMMON.explosives.smokeGrenadeCloudDiameter.get();
         float cookTime = (float) maxCookTime / 20;
-        tooltip.add(Component.translatable("info.cgm.smoke_duration", ChatFormatting.WHITE + ItemStack.ATTRIBUTE_MODIFIER_FORMAT.format(smokeDuration)).withStyle(ChatFormatting.GRAY));
-        tooltip.add(Component.translatable("info.cgm.smoke_diameter", ChatFormatting.WHITE + ItemStack.ATTRIBUTE_MODIFIER_FORMAT.format(smokeDiameter)).withStyle(ChatFormatting.GRAY));
-        tooltip.add(Component.translatable("info.cgm.fuse", ChatFormatting.WHITE + ItemStack.ATTRIBUTE_MODIFIER_FORMAT.format(cookTime)).withStyle(ChatFormatting.GRAY));
+        if(Screen.hasControlDown())
+        {
+            tooltip.add(Component.translatable("info.cgm.gun_details").withStyle(ChatFormatting.GOLD));
+            tooltip.add(Component.translatable("info.cgm.smoke_duration", ChatFormatting.WHITE + ItemStack.ATTRIBUTE_MODIFIER_FORMAT.format(smokeDuration)).withStyle(ChatFormatting.GRAY));
+            tooltip.add(Component.translatable("info.cgm.smoke_diameter", ChatFormatting.WHITE + ItemStack.ATTRIBUTE_MODIFIER_FORMAT.format(smokeDiameter)).withStyle(ChatFormatting.GRAY));
+            tooltip.add(Component.translatable("info.cgm.fuse", ChatFormatting.WHITE + ItemStack.ATTRIBUTE_MODIFIER_FORMAT.format(cookTime)).withStyle(ChatFormatting.GRAY));
+        }
+        else
+        {
+            tooltip.add(Component.translatable("info.cgm.gun_details_help").withStyle(ChatFormatting.GOLD));
+        }
     }
 
     @Override
